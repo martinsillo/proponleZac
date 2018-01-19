2704cceecd615c1f959c460e5c6117442d497f06
Martin Gamboa <martinsillo.gamboa@gmail.com>
2018-01-19 15:38:09 -0600

    se agrega funcion para guardar debate

--- js/proponle.js
+++ js/proponle.js
@@ -1,9 +1,17 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
+    'use strict';
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
     }
+}
+
+function guardarDebate() {
+    'use strict';
+
+
+
+    return false;
 }
