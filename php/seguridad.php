2ef487fad6f0b2902635ceb17dd29a0037eaa62c
martinsillo <martinsillo.gamboa@gmail.com>
2018-01-23 14:58:58 -0600

    se agrega una key_page

--- php/seguridad.php
+++ php/seguridad.php
@@ -1,4 +1,13 @@
 <?php
+
 session_start();
-$_SESSION['user_id'] = 1;
-$_SESSION['perfil'] = 2;
+// para propositos de desarrollo, se crea de forma temporal la variable de sesion key_page
+$_SESSION['key_page'] = md5('secret word');
+
+if(!isset($_SESSION['key_page'])){
+    session_destroy();
+}else{
+    // declaramos las variables de sesion
+    $_SESSION['user_id'] = 1;
+    $_SESSION['perfil'] = 2;
+}
