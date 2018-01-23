9e58f5111e1f2a164dba9867e3c4602c6541e500
martinsillo <martinsillo.gamboa@gmail.com>
2018-01-23 09:58:39 -0600

    new file

---
+++ index.php
@@ -0,0 +1,109 @@
+<!Doctype HTML>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Proponle a Zacatecas">
+    <meta name="keywords" content="Zacatecas, Propuestas, Avanzamos, Avanzemos, Debate">
+    <meta name="author" content="Gobierno del Estado de Zacatecas">
+    <title>Proponle a Zacatecas</title>
+    <!-- Hojas de Estilo -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/icon.ico" />
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300" rel="stylesheet">
+    <link rel="stylesheet" href="css/proponle.css">
+    <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+    <body>
+        <div class="barra_top">
+            Transparencia &nbsp;|&nbsp; Datos Abiertos &nbsp;|&nbsp; Foros Locales &nbsp;|&nbsp; <i class="fa fa-facebook-square" aria-hidden="true"></i>&nbsp;&nbsp;<i class="fa fa-twitter-square" aria-hidden="true"></i>&nbsp;
+        </div>
+        <div class="contenido" style="padding-left: 10%; padding-right: 10%;">
+            <div class="row" style="padding: 1px 1px 1px 1px;">
+                <div class="col-md-6">
+                    <img src="img/logo2.png"  class="img-responsive" style="margin: 0 auto;">
+                </div>
+                <div class="col-md-6" style="text-align:right; padding-right: 20%">
+                    <br><br>
+                     <button type="button" class="btn btn-outline btn-default">Entrar</button>
+                   <a type="button" class="btn btn-outline btn-primary" href="registro.html">Registrarse</a>
+                </div>
+            </div>
+<!-- Barra de Navegación -->
+            <br>
+            </div>
+<div class="topnav" id="myTopnav">
+  <a href="#" class="active">Inicio</a>
+  <a href="debates.php">Debates</a>
+  <a href="#">Propuestas</a>
+  <a href="#">Sondeos/Votaciones</a>
+  <a href="#">Documentos</a>
+  <a href="#">Más Información</a>
+  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+</div>
+    <div style="text-align: center; padding-top: 25px; padding-left: 10%; padding-right: 10%;">
+        <br>
+        <button type="button" class="btn btn-danger2">Haz tu Propuesta Ciudadana</button>
+        <br>
+        <h2>¿TIENES UNA IDEA PARA CAMBIAR ZACATECAS?</h2>
+        <h4>El Gobierno del Estado llevará a cabo tu propuesta si consigues que pase a votación ciudadana. Lorem Ipsum Dolor Est.</h4>
+        <br>
+        <button type="button" class="btn btn-outline btn-primary btn-lg" >Crea una Propuesta</button>
+    </div>
+<hr>
+    <div style="padding-left: 10%; padding-right: 10%;">
+        <div class="row">
+            <div class="col-md-4">pic 1</div>
+            <div class="col-md-4">pic 2</div>
+            <div class="col-md-4">pic 3</div>
+        </div>
+    </div>
+
+   <div class="pie_pag">
+      <div class="container">
+        <div class="row" style="padding: 9px 1px 5px 1px;">
+            <div class="col-md-3"><img src="img/logo_zacatecas.png" class="img-responsive"></div>
+            <div class="col-md-3">
+            <h4><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;Información</h4>
+                <ul>
+                    <li><a href="politica_privacidad.html">Politica de Privacidad</a></li>
+                    <li>Condiciones de uso</li>
+                    <li>Soporte Técnico</li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h4><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;Contacto</h4>
+
+Coordinación Estatal de Planeación<br>
+Circuito Cerro del Gato s/n<br>
+Colonia Ciudad Gobierno<br>
+Edificio H | Segundo Piso<br>
+C.P. 98160<br>
+Zacatecas, Zac.<br>
+            </div>
+            <div class="col-md-3">
+
+                <h4><i class="fa fa-phone-square" aria-hidden="true"></i>&nbsp;Teléfonos</h4>
+                <ul>
+                    <li>922 3005 Cruz Roja Mexicana</li>
+                    <li>922 5350 Bomberos</li>
+                    <li>922 0180 Policía Estatal Preventiva</li>
+                    <li>924 5320 Tránsito del Estado</li>
+                    <li>491 5000 Ciudad Gobierno</li>
+                    <li>911 Emergencias</li>
+                    <li>089 Denuncia Anónima</li>
+                </ul>
+            </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/proponle.js?v=1.0.0"></script>
+    </body>
+</html>
---
+++ politica_privacidad.php
@@ -0,0 +1,109 @@
+<!Doctype HTML>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Proponle a Zacatecas">
+    <meta name="keywords" content="Zacatecas, Propuestas, Avanzamos, Avanzemos, Debate">
+    <meta name="author" content="Gobierno del Estado de Zacatecas">
+    <title>Proponle a Zacatecas</title>
+    <!-- Hojas de Estilo -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/icon.ico" />
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300" rel="stylesheet">
+    <link rel="stylesheet" href="css/proponle.css">
+    <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+    <body>
+
+        <div class="barra_top">
+            Transparencia &nbsp;|&nbsp; Datos Abiertos &nbsp;|&nbsp; Foros Locales &nbsp;|&nbsp; <i class="fa fa-facebook-square" aria-hidden="true"></i>&nbsp;&nbsp;<i class="fa fa-twitter-square" aria-hidden="true"></i>&nbsp;
+        </div>
+        <div class="contenido" style="padding-left: 10%; padding-right: 10%;">
+            <div class="row" style="padding: 1px 1px 1px 1px;">
+                <div class="col-md-6">
+                    <img src="img/logo2.png"  class="img-responsive" style="margin: 0 auto;">
+                </div>
+                <div class="col-md-6" style="text-align:right; padding-right: 20%">
+                    <br><br>
+                     <button type="button" class="btn btn-outline btn-default">Entrar</button>
+                   <a type="button" class="btn btn-outline btn-primary" href="registro.html">Registrarse</a>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="topnav" id="myTopnav">
+  <a href="#" class="active">Inicio</a>
+  <a href="#">Debates</a>
+  <a href="#">Propuestas</a>
+  <a href="#">Sondeos/Votaciones</a>
+  <a href="#">Documentos</a>
+  <a href="#">Más Información</a>
+  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+</div>
+    <div style="padding-left: 10%; padding-right: 10%;">
+        <div class="row">
+            <div class="col-md-3"><br><h2>Politica de Privacidad</h2><br><h3>Aviso de protección de datos</h3></div>
+            <div class="col-md-3"><br><button type="button" class="btn btn-outline btn-info"><i class="fa fa-print" aria-hidden="true"></i> Imprimir esta información</button></div>
+        </div>
+
+        <p>
+        La navegación por la informacion disponible en el Portal de Gobierno Abierto del Ayuntamiento de Madrid es anónima.
+Para utilizar los servicios contenidos en el Portal de Gobierno Abierto del Ayuntamiento de Madrid el usuario deberá darse de alta y proporcionar previamente los datos de carácter personal segun la informacion especifica que consta en cada tipo de alta.
+Los datos aportados serán incorporados y tratados por el Ayuntamiento de Madrid de acuerdo con la descripción del fichero siguiente:
+Nombre del fichero/tratamiento: GESTIÓN DE PROCESOS PARTICIPATIVOS
+Finalidad del fichero/tratamiento: Gestionar los procesos participativos para el control de la habilitación de las personas que participan en los mismos y recuento meramente numérico y estadístico de los resultados derivados de los procesos de participación ciudadana
+Órgano responsable: DIRECCIÓN GENERAL DE PARTICIPACIÓN CIUDADANA (c/ Alcalá 45, 28014-Madrid)
+El interesado podrá ejercer los derechos de acceso, rectificación, cancelación y oposición, ante el órgano responsable indicado todo lo cual se informa en el cumplimiento del artículo 5 de la Ley Orgánica 15/1999, de 13 de diciembre, de Protección de Datos de Carácter Personal.
+Como principio general, este sitio web no comparte ni revela información obtenida, excepto cuando haya sido autorizada por el usuario, o la informacion sea requerida por la autoridad judicial, ministerio fiscal o la policia judicial, o se de alguno de los supuestos regulados en el artículo 11 de la Ley Orgánica 15/1999, de 13 de diciembre, de Protección de Datos de Carácter Personal.
+
+        </p>
+    </div>
+<div class="pie_pag">
+      <div class="container">
+        <div class="row" style="padding: 9px 1px 5px 1px;">
+            <div class="col-md-3"><img src="img/logo_zacatecas.png" class="img-responsive"></div>
+            <div class="col-md-3">
+            <h4><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;Información</h4>
+                <ul>
+                    <li>Politica de Privacidad</li>
+                    <li>Condiciones de uso</li>
+                    <li>Soporte Técnico</li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h4><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;Contacto</h4>
+
+Coordinación Estatal de Planeación<br>
+Circuito Cerro del Gato s/n<br>
+Colonia Ciudad Gobierno<br>
+Edificio H | Segundo Piso<br>
+C.P. 98160<br>
+Zacatecas, Zac.<br>
+            </div>
+            <div class="col-md-3">
+
+                <h4><i class="fa fa-phone-square" aria-hidden="true"></i>&nbsp;Teléfonos</h4>
+                <ul>
+                    <li>922 3005 Cruz Roja Mexicana</li>
+                    <li>922 5350 Bomberos</li>
+                    <li>922 0180 Policía Estatal Preventiva</li>
+                    <li>924 5320 Tránsito del Estado</li>
+                    <li>491 5000 Ciudad Gobierno</li>
+                    <li>911 Emergencias</li>
+                    <li>089 Denuncia Anónima</li>
+                </ul>
+            </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/proponle.js?v=1.0.0"></script>
+    </body>
+</html>
